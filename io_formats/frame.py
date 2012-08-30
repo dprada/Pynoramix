dae06cabf2af42f5a6910d052ec846adf09ced07
Diego Prada <Diego.Prada@frias.uni-freiburg.de>
2012-08-30 20:31:16 +0200

    Reading trr files, in process.

--- io_formats/frame.py
+++ io_formats/frame.py
@@ -4,6 +4,7 @@ class cl_frame():
         self.time=None
         self.step=None
         self.precision=None
+        self.lam=None                    # Coming from trr files... what is this?
         self.model=None
         self.coors=[]
         self.box=zeros(shape=(3,3),order='Fortran')
